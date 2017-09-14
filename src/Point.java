a991f537e9f197db9973e718977908b29474a92c
bertins <bertins@acajou10.iut-infobio.priv.univ-lille1.fr>
2017-09-14 11:38:24 +0200

    classe Point

---
+++ src/Point.java
@@ -0,0 +1,14 @@
+
+public class Point {
+	private Coordonnees coord=new Coordonnees(0,0);
+
+	public Point(int x, int y){
+		coord.setX(x);
+		coord.setY(y);
+	}
+
+
+	public void avant(int distance){
+		
+	}
+}
