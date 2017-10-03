6dcd4ad6c1e4c7424cce87c77f695b120a013347
PC <PC@192.168.1.46>
2017-10-03 19:02:07 +0200

    Adaptation de la Classe Ligne C

--- src/LigneC.java
+++ src/LigneC.java
@@ -1,10 +1,20 @@
 
 public class LigneC {
-	String action;
-	String val;
+	public String action;
+	public String val;
+	public String val2;
+	
+	public LigneC(String ac){
+		this.action = ac;
+	}
 	
 	public LigneC(String ac, String val){
 		this.action = ac;
 		this.val = val;
+	}
+	public LigneC(String ac, String val, String val2){
+		this.action = ac;
+		this.val = val;
+		this.val2 = val2;
 	}
 }
