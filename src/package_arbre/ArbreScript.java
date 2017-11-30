d43637c95a3a39615c82ab26238aab9583386e9b
Sarah <Sarah@pc-sarah>
2017-11-30 11:47:49 +0100

    début ArbreScript

---
+++ src/package_arbre/ArbreScript.java
@@ -0,0 +1,24 @@
+package package_arbre;
+
+import java.util.List;
+
+public class ArbreScript extends ArbreMot {
+	
+	private List<ArbreMot> liste;
+	private ArbreScript papa;
+	
+	public ArbreScript(){}
+	
+	@Override
+	public void add(ArbreMot AM) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void executer() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
+}
